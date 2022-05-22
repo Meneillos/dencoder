40b525528955d23a0eff912b6029c1861451fe2c
Arnau Carrasco <arnau.carrasco@gmail.com>
2022-05-22 20:59:24 +0200

    added some comments

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"os"
 )
 
+// Encode a string in standard base64 format.
 func encode(str string) string {
 	return base64.StdEncoding.EncodeToString([]byte(str))
 }
 
+// Encode an already encoded string.
 func decode(str string) string {
 	originalStr, err := base64.StdEncoding.DecodeString(str)
 	if err != nil {
@@ -19,12 +21,14 @@ func decode(str string) string {
 	return string(originalStr)
 }
 
+// Print usage and exit.
 func usage() {
 	fmt.Println("USAGE:")
 	fmt.Println("dencoder [-encode / -decode] string")
 	os.Exit(-1)
 }
 
+// Main function.
 func main() {
 	if len(os.Args) != 3 {
 		usage()
